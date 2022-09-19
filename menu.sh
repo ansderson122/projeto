8cda8e5e25f027f1d5e3986c06990fecdff50932
Your Name <you@example.com>
2022-09-19 15:38:09 -0300

    primeiro commit do menu inicial

---
+++ menu.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+echo $(bash ex/ex1.sh 4 6)
+red="\033[0;31m"
+redf="\033[0m"
+gre="\033[0;32m"
+gref="\033[0m"
+echo -e "$red penis vermelho $redf"
+
+echo -e "$gre
+
+     _                       _____                                        
+ ___/__)    /)     /)       (, /   )                                      
+(, /   _   (/_   _(/  _      _/__ / __  ____   __  _  ___   _   _  _   ___
+  /   (_(_/_)   (_(__(/_     /     / (_(_)(_/_/ (_(_(_// (_(_(_(__(_(_(_) 
+ (_____                   ) /            .-/                              
+        )                (_/            (_/                               
+$gref
+"
+echo "
+
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒░░░ LISTA 01 ░░░▒▒▒▓▓▓▓▓▓▓▓▓▓▓
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+"
+echo "Informe qual das 12 questões você deseja acessar:"
+read op
+if [ op -eq 1 ]
+do
+
+echo "Você escolheu a questão 1"
+echo "
+Exercício 1 Elaborar um script que receba dois números como parâmetros e como resultado da sua execução exiba as seguintes informações: - O maior dos números recebidos; - O somatório dos números recebidos. Exemplo: 
+./meuscript.sh 23 14 O maior número é 23. A soma dos números é 37.
+"
+
+fi
+if [ op -eq 2 ]
+do
+
+"Você escolheu a questão 1"
+echo $(bash ex/ex1.sh 4 6)
+fi
+if [ op -eq 3 ]
+do
+
+"Você escolheu a questão 1"
+echo $(bash ex/ex1.sh 4 6)
+fi
+if [ op -eq 4 ]
+do
+
+"Você escolheu a questão 1"
+echo $(bash ex/ex1.sh 4 6)
+fi
+if [ op -eq 5 ]
+do
+
+"Você escolheu a questão 1"
+echo $(bash ex/ex1.sh 4 6)
+fi
+if [ op -eq 6 ]
+do
+
+"Você escolheu a questão 1"
+echo $(bash ex/ex1.sh 4 6)
+fi
+if [ op -eq 7 ]
+do
+
+"Você escolheu a questão 1"
+echo $(bash ex/ex1.sh 4 6)
+fi
+if [ op -eq 8 ]
+do
+
+"Você escolheu a questão 1"
+echo $(bash ex/ex1.sh 4 6)
+fi
+if [ op -eq 9 ]
+do
+
+"Você escolheu a questão 1"
+echo $(bash ex/ex1.sh 4 6)
+fi
+if [ op -eq 10 ]
+do
+
+"Você escolheu a questão 1"
+echo $(bash ex/ex1.sh 4 6)
+fi
+if [ op -eq 11 ]
+do
+
+"Você escolheu a questão 1"
+echo $(bash ex/ex1.sh 4 6)
+fi
+if [ op -eq 12 ]
+do
+
+"Você escolheu a questão 1"
+echo $(bash ex/ex1.sh 4 6)
+fi
