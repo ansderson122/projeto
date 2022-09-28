2bef4229efe36c5080335bf3b2499ecd22f225a5
Julyanderson Alves <ansderson84001@gmail.com>
2022-09-28 10:00:58 -0300

    design da questao 5

--- ex/ex5.sh
+++ ex/ex5.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo -e "$red Exercício 5$redf Elaborar um script que receba um nome de arquivo como parâmetro e verifique se esse arquivo existe. Em caso afirmativo, imprimir o seu tamanho em bytes. \n 
-$red Exemplo 1:$redf ./meuscript.sh /etc/fstab \n O arquivo /etc/fstab existe e tem 861 bytes. \n $redd Exemplo 2:$redf ./meuscript.sh /etc/arqqualquer\n O arquivo /etc/arqqualquer não existe\n"
+$red Exemplo 1:$redf ./meuscript.sh /etc/fstab \n O arquivo /etc/fstab existe e tem 861 bytes. \n\n$red Exemplo 2:$redf ./meuscript.sh /etc/arqqualquer\n O arquivo /etc/arqqualquer não existe.\n"
     
 read -p "Digite um nome de arquivo" a
  
