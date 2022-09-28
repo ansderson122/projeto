906e38b05807a6aec35b4cbe5a78122c298eb6e0
Julyanderson Alves <ansderson84001@gmail.com>
2022-09-28 15:14:16 -0300

    design inicial da 9

--- ex/ex9.sh
+++ ex/ex9.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
- echo "Exercício 9 Desenvolva um script que receba uma lista de palavras como parâmetros e exiba, como resultado, essas palavras ordenadas. Exemplo: 
+ echo -e "$redN Exercício 9$f Desenvolva um script que receba uma lista de palavras como parâmetros e exiba, como resultado, essas palavras ordenadas. Exemplo: 
     ordena_palavras.sh casa bola abacaxi abacaxi bola casa"
 
-read -p "Digite a lista: " a
-echo $* | tr ' ' '\n' | sort -n | tr '\n' '\n'; echo 
-echo
+echo -e -n $gre"Digite a lista: "$f
+read a
+v=$(echo $a | tr ' ' '\n' | sort -n | tr '\n' ' ')
+echo -e "$blu$v$f\n"
