bb725ce7b4b8c51dbec1328d8d846ed05d1b1a04
Julyanderson Alves <ansderson84001@gmail.com>
2022-09-28 09:54:27 -0300

    design da questao 3

--- ex/ex3.sh
+++ ex/ex3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "Exercício 3 Escreva um script que receba como parâmetro o caminho de um diretório e liste seus arquivos e diretórios um a um. Se for diretório, deve escrever '(dir)' ao lado do diretório. Exemplo 1: 
-    ./meuscript.sh /tmp arq1 arq2 dir1 (dir) xara dir2 (dir) "
+echo -e "$red Exercício 3$redf Escreva um script que receba como parâmetro o caminho de um diretório e liste seus arquivos e diretórios um a um. Se for diretório, deve escrever '(dir)' ao lado do diretório."
+echo -e "$red Exemplo 1:$redf ./meuscript.sh /tmp arq1 arq2 dir1 (dir) xara dir2 (dir) \n"
  
  read -p "Digite um diretório" a
 
