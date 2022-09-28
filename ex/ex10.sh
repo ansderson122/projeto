0cc38d42a3cf4168859cdf16b85d4ede5799ca54
Julyanderson Alves <ansderson84001@gmail.com>
2022-09-28 15:52:48 -0300

    design inicial da 10

--- ex/ex10.sh
+++ ex/ex10.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
-echo "Exercício 10 Desenvolva um script que receba palavras interativamente e, a cada palavra recebida, exiba, como resultado, essas palavras ordenadas. Ao ser informada a palavra "sair", o script é finalizado. Exemplo: 
-    ordena_palavras_interativamente.sh Informe a palavra a ser incluída na lista ordenada: casa casa Informe a palavra a ser incluída na lista ordenada: abacaxi abacaxi casa Informe a palavra a ser incluída na lista ordenada: bola abacaxi bola casa Informe a palavra a ser incluída na lista ordenada: sair "
+echo -e "$redN Exercício 10$f Desenvolva um script que receba palavras interativamente e, a cada palavra recebida, exiba, como resultado, essas palavras ordenadas. Ao ser informada a palavra sair, o script é finalizado.$red\nExemplo:$f ordena_palavras_interativamente.sh\n$blu Informe a palavra a ser incluída na lista ordenada:$f casa\n casa\n$blu Informe a palavra a ser incluída na lista ordenada:$f abacaxi\n abacaxi casa\n$blu Informe a palavra a ser incluída na lista ordenada:$f bola\n abacaxi bola casa\n$blu Informe a palavra a ser incluída na lista ordenada:$f sair  \n"
 
 s="continua"
 while test "$s" != "sair" ; do
-    read -p "Informe a palavra a ser incluida na lista ordenada: " palavra
-    
+    echo -e -n $gre"Informe a palavra a ser incluida na lista ordenada: "
+    read palavra
+    echo -e $f
     if [ "$palavra" = "sair" ] ; then
         s="sair"
         continue 
     fi
     
     lista="$lista $palavra"
-    echo $lista | tr ' ' '\n' | sort -n | tr '\n' '\n'; 
-    echo " "
+    echo $lista | tr ' ' '\n' | sort -n | tr '\n' ' '; 
 done
 
 echo
